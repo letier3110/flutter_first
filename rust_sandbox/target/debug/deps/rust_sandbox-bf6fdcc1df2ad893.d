5990d9bdf20637c97864a287bb8dd59a66db96cc
letier3110 <letier3110@gmail.com>
2022-01-16 20:21:03 +0200

    initial commit

--- rust_sandbox/target/debug/deps/rust_sandbox-bf6fdcc1df2ad893.d
+++
@@ -1,5 +0,0 @@
-/Users/antonchaika/job/rust_sandbox/target/debug/deps/rust_sandbox-bf6fdcc1df2ad893: src/main.rs
-
-/Users/antonchaika/job/rust_sandbox/target/debug/deps/rust_sandbox-bf6fdcc1df2ad893.d: src/main.rs
-
-src/main.rs:
--- rust_sandbox/target/debug/rust_sandbox.d
+++
@@ -1,1 +0,0 @@
-/Users/antonchaika/job/rust_sandbox/target/debug/rust_sandbox: /Users/antonchaika/job/rust_sandbox/src/main.rs
